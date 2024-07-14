788daaf35253436dc04d5443847cb03ff6813413
ahmad <hadi.mirzaie300@gmail.com>
2024-07-14 13:57:13 +0330

    remove multithread mode due to rate limit issue

--- src/Methods/topSellNfts/topSellNfts.php
+++ src/Methods/topSellNfts/topSellNfts.php
@@ -44,11 +44,10 @@ class topSellNfts{
 
         $weiToEther = new weiToEther();
 
-        if($mode === "singleThread"){
            
-            $filterData = array_map(function($log) use($topSellNftsJson){
+        $filterData = array_map(function($log) use($topSellNftsJson){
                 
-                return array_map(function($logs) use($topSellNftsJson){
+            return array_map(function($logs) use($topSellNftsJson){
                         
                         $data = $topSellNftsJson->getTopSellNftsJson($logs);
                     
@@ -59,115 +58,22 @@ class topSellNfts{
                                    $logs = $result['value'];
                                    return $logs;
 
-                                   }
-                },$log);
+                        }
+            },$log);
 
-            },$trxHashandIds);
+        },$trxHashandIds);
             
-            $result = array_map(function($log) use($weiToEther) {
-
-                $sum = array_sum(array_map('hexdec',$log));
-               
-                        $nftSumPrice = $weiToEther->getWeiToEther([$sum]);
-        
-                        return $nftSumPrice;
-        
-            },$filterData);
-        }
-
-        elseif($mode === "multiThread"){
-
-          die('stopppp');
-
-            /*
-            //create the multiple cURL handle
-            $cmi = curl_multi_init();
-
-            $filter = array_map(function($logs) use($topSellNftsJson,&$cmi){
-
-               return array_map(function($log) use($topSellNftsJson,&$cmi){
-
-                    
-                    $data = $topSellNftsJson->getTopSellNftsJson($log);
-
-                    $ci = curl_init();
-
-                    // set URL and other appropriate options
-                    curl_setopt($ci, CURLOPT_URL, $this->provider);
-                    curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
-                    curl_setopt($ci, CURLOPT_HTTPHEADER, array(
-                        'Content-Type: application/json'
-                    ));
-                    curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, 0);
-
-                    $json_data = json_encode($data);
-
-                    # Send the cURL request with the JSON-RPC request in the body
-                    curl_setopt($ci, CURLOPT_POST, true);
-                    curl_setopt($ci, CURLOPT_POSTFIELDS, $json_data);
-
-                    // set Proxy for the Request
-                    if( $this->proxy !== null){
-                        curl_setopt($ci, CURLOPT_PROXY, $this->proxy);
-                    }   
-        
-                    //add the two handles
-                    curl_multi_add_handle($cmi,$ci);
-                    
-                    
-                    return $ci;
-                },$logs);
-            },$trxHashandIds);     
-
-
-            //execute the multi handle
-            do {
-                
-               $status = curl_multi_exec($cmi, $active);
-                if ($active) {
-                    // Wait a short time for more activity
-                    curl_multi_select($cmi);
-                }
-                sleep(2);
-                
-            } while ($active && $status == CURLM_OK);
-
-
-            $getValues = array_map(function($logs){
-               return array_map(function($log){
-
-                    $re = array(curl_multi_getcontent($log));
-                    $result = json_decode($re[0],true);
-
-                    if(isset($result['result'])){
-
-                        $value = $result['result']['value'];
-                        
-                        return $value;
-                    }
-
-                    curl_multi_remove_handle($cmi, $ci);
-                    curl_multi_close($cmi);
-
-                },$logs);    
-            },$filter);
-
-          
-            $result = array_map(function($log) use($weiToEther) {
+        $result = array_map(function($log) use($weiToEther) {
 
                 $sum = array_sum(array_map('hexdec',$log));
                
                 $nftSumPrice = $weiToEther->getWeiToEther([$sum]);
         
                 return $nftSumPrice;
-        
-            },$getValues);
 
-            */
+        },$filterData);
 
-        }
 
-       
         arsort($result);
 
 
